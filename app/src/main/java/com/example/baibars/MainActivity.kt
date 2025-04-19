f0a52d23043dbcfe825a73285a9b9bf5b8c0c235
Emrullah Aktürk <akturk.emrullah@hotmail.com>
2025-04-19 21:27:09 +0300

    MainActivity.kt updated

--- app/src/main/java/com/example/baibars/MainActivity.kt
+++ app/src/main/java/com/example/baibars/MainActivity.kt
@@ -13,16 +13,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
 
         // NavHostFragment kurulumu
         val navHostFragment = supportFragmentManager
